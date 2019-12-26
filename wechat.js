35f01666c42170e9969ae5d151482a2fd2094cd4
shiweiwei <834879034@qq.com>
2019-12-26 16:28:41 +0800

    读写文件，存储，删除文件等

--- wechat.js
+++ wechat.js
@@ -1,10 +1,34 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const fs = require('fs');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const dayjs = require('dayjs');
 
+// 所有用户列表
 const userList = []
+// 聊天记录
+let recode = ''
 
 io.on('connection', function (socket) {
+	// 判断是否存在文件夹
+	fs.exists('./images', (exists) => {
+		if (!exists) {
+			fs.mkdirSync("./images")
+		}
+	})
+
+	fs.exists('./files', (exists) => {
+		if (!exists) {
+			fs.mkdirSync("./files")
+		}
+	})
+
+
+	// 登陆
+	socket.on('userLogin', () => {
+		socket.emit('handleLogin')
+	})
+
 	// console.log('客户端已经连接了');
 	socket.on('join', msg => {
 		let result = userList.find(user => user.userId == msg.userId)
@@ -28,6 +52,27 @@ io.on('connection', function (socket) {
 	socket.on('joinGroup', function (msg) {
 		// socket.broadcast.emit('broadcast', msg);
 		io.emit('broadcast', msg)
+		// 聊天内容，创建写入文件，图片
+		let time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss')
+		if (msg.type == 'img') {
+			recode = `时间: ${time}，\nuserId: ${msg.userId}，\n内容：['图片']\n\r`
+			let base64Data = msg.content.replace(/^data:image\/\w+;base64,/, "");
+			let decodeImg = Buffer.from(base64Data, 'base64')
+			fs.writeFile(`./images/${msg.userId}_${Date.now()}.png`, decodeImg, err => {
+				if (err) {
+					throw '写入图片失败！'
+				}
+			});
+		} else {
+			recode = `时间: ${time}，\nuserId: ${msg.userId}，\n内容：${msg.content}\n\r`
+		}
+		// 追加文件内容
+		fs.appendFile('./files/record.txt', recode, {}, err => {
+			if (err) {
+				throw '写入文件失败！'
+			}
+		})
+
 		if (userList.length) {
 			let i = userList.findIndex(item => item.userId == msg.userId)
 			if (i > -1) {
@@ -53,4 +98,39 @@ io.on('connection', function (socket) {
 
 http.listen(3000, function () {
 	console.log('服务器已启动，监听端口为3000');
+
+	// 先删除文件夹下的所有文件，再删除文件夹
+	fs.exists('./files', (exists) => {
+		if (exists) {
+			// console.log("文件目录存在")
+			let fileList = fs.readdirSync('./files')
+			if (fileList.length) {
+				fileList.forEach(file => {
+					fs.unlinkSync('./files/' + file);
+				})
+			} else {
+				fs.rmdirSync('./files')
+			}
+		}
+		if (!exists) {
+			// console.log("文件目录不存在")
+		}
+	})
+
+	fs.exists('./images', (exists) => {
+		if (exists) {
+			// console.log("图片目录存在")
+			let imgList = fs.readdirSync('./images')
+			if (imgList.length) {
+				imgList.forEach(img => {
+					fs.unlinkSync('./images/' + img)
+				})
+			} else {
+				fs.rmdirSync('./images')
+			}
+		}
+		if (!exists) {
+			// console.log("图片目录不存在")
+		}
+	})
 });
